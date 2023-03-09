187ae50e256e74237caa74cc60fe1fe1e25ed8ad
M.H. van der Velde <m.h.van.der.velde.1@student.rug.nl>
2023-03-09 15:32:33 +0100

    testing count functions

--- manipulation_testing.py
+++ manipulation_testing.py
@@ -17,6 +17,26 @@ def get_totals(totals, plur_score):
 
 def profile_ranking(profile, alternatives):
     totals = {x: 0 for x in alternatives}
+    alt_before_8 = {x: 0 for x in alternatives}
+    for x in profile:
+        try:
+            idx = x[1].index(8)
+            for i in range(idx):
+                if not isinstance(x[1][i], int):
+                    set_end = x[1].index('}')
+                    if set_end < idx:
+                        for j in range(i+1, set_end):
+                            alt_before_8[x[1][j]] += x[0] * (1/2)
+                    else:
+                        for j in range(i+1, idx):
+                            alt_before_8[x[1][j]] += x[0] * (1/2)
+                else:
+                    alt_before_8[x[1][i]] += x[0]
+        except ValueError:
+            continue
+    print(f"{alt_before_8=}")
+    totals = {x: 0 for x in alternatives}
+    count = 0
     for i in range(10):
         plur_score = plurality(profile, alternatives)
         totals = get_totals(totals, plur_score)
@@ -28,6 +48,7 @@ def profile_ranking(profile, alternatives):
                     popped = x[1].pop(0)
         profile = [x for x in profile if x[1]]
     print(f"{totals=}")
+    print(f"{count=}")
     return
 
 
