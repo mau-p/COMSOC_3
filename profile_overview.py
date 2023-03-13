b6e607a5916d99565f5c7b7ec20b7ce5efc59422
M.H. van der Velde <m.h.van.der.velde.1@student.rug.nl>
2023-03-13 17:30:48 +0100

    commented code

--- profile_overview.py
+++ profile_overview.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 
+
 def find_elem(lst, elem):
     for x in lst:
         if elem in x:
@@ -12,19 +13,28 @@ def data_overview(profile, alternatives, winner=None):
     overview = pd.DataFrame(columns=[x for x in range(1,11)], index=[x for (x,) in alternatives])
     overview = overview.fillna(0)
     for voter in profile:
+        idx_winner = None
         if winner:
             idx_winner = find_elem(voter[1], winner)
-        else:
-            idx_winner = None
         for pref in voter[1]:
-            if idx_winner and voter[1].index(pref) > idx_winner:
-                continue
-            idx = voter[1].index(pref)+1
-            if len(pref) > 1:
-                for x in pref:
-                    overview.loc[x, idx] += float(voter[0]*(1/2))
+            if idx_winner == 0:
+                break
+            elif idx_winner != None:
+                if voter[1].index(pref) < idx_winner:
+                    idx = voter[1].index(pref)+1
+                    if len(pref) > 1:
+                        for x in pref:
+                            if x:
+                                overview.loc[x, idx] += float(voter[0]*(1/2))
+                    else:
+                        overview.loc[pref[0], idx] += voter[0]
             else:
-                overview.loc[pref[0], idx] += voter[0]
+                idx = voter[1].index(pref)+1
+                if len(pref) > 1:
+                    for x in pref:
+                        if x:
+                            overview.loc[x, idx] += float(voter[0]*(1/2))
+                else:
+                    overview.loc[pref[0], idx] += voter[0]
     overview['total'] = overview.sum(axis=1)
-    print(overview)
-    return
+    return overview
